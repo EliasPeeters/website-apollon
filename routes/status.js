860ee822372fa3e73830afda7fd028c9dd86fbc6
Elias Peeters <Elias.peeters@otto.de>
2021-10-16 18:46:54 +0200

    Removed test message on /

--- routes/status.js
+++ routes/status.js
@@ -1,7 +1,3 @@
 app.get('/', async (req, res) => {
     res.send('Apollon');
-    let subscribers = await connection.asyncquery('SELECT * FROM telegramBotSubs');
-    for (let i = 0; i < subscribers.length; i++) {
-        bot.sendMessage(subscribers[i].tb_chatID, 'Good morning');
-    }
 })
